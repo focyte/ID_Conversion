3a94da16cb408814ad624a9264b10d6ff64f6112
John Foster <focyte@gmail.com>
2022-08-30 20:27:52 +0100

    Update ENSEMBL_to_SYMBOL.r

--- ENSEMBL_to_SYMBOL.r
+++ ENSEMBL_to_SYMBOL.r
@@ -1,7 +1,12 @@
 # John @ Focyte created 30/08/2022 v.1.1
 # Script converts ENSEMBL IDs to gene symbol names
 
-### Load required packages
+# If needed, this is how to install BiocManager to load the required packages below
+if (!require("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+BiocManager::install(version = "3.15")
+
+# Load required packages
 
 BiocManager::install("org.Hs.eg.db")
 BiocManager::install("AnnotationDbi")
@@ -9,9 +14,18 @@ library("org.Hs.eg.db")
 
 ### Load example data from https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE149204 
 ### GSE149204_Nanopore_sequencing_readcounts.txt
+counts.df <- read.table("GSE149204_Nanopore_sequencing_readcounts.txt", header = TRUE, sep = "", dec = ".")
+counts.df <- as.data.frame(counts.df)
 
-counts.df <- as.data.frame("GSE149204_Nanopore_sequencing_readcounts.txt")
+# Convert the ENSEMBL IDs into the row names
 counts.df2 <- counts.df[,-1]
 rownames(counts.df2) <- counts.df[,1]
-counts.df2$symbol <- mapIds(org.Hs.eg.db, keys = rownames(counts.df2), keytype = "ENSEMBL", column = "SYMBOL")
+
+# Convert ENSEMBL to Symbol IDs and write the file to working directory
+counts.df2$symbol <- mapIds(org.Hs.eg.db, 
+                           keys = as.character(rownames(counts.df2)), 
+                           keytype = "ENSEMBL", 
+                           column = "SYMBOL",
+                           multiVals = "first")
 write.csv(counts.df2,"counts.csv")
+
